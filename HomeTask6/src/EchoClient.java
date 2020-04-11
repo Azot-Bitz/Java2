f100417e35bc1d879960ffecf1a43299ee6bbb81
Vasily Kulikov <zel.slonik17@gmail.com>
2020-04-11 20:10:22 +0300

    Шестое домашнее задание 2

--- HomeTask6/src/EchoClient.java
+++ HomeTask6/src/EchoClient.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoClient {
+    public static void main(String[] args) throws RuntimeException, IOException {
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 4444);
+            Scanner in =  new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String msg = in.nextLine();
+                        System.out.println("Сервер: " + msg);
+                    }
+                }
+            }).start();
+            while (true) {
+                System.out.println("Напишите свое сообщение...");
+                String msg = sc.nextLine();
+                if(msg.equals("/end")){
+                    socket.close();
+                    break;
+                }
+                out.println(msg);
+            }
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- HomeTask6/src/EchoServer.java
+++ HomeTask6/src/EchoServer.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
+
+    public static void main(String[] args) {
+        Scanner is = new Scanner(System.in);
+        Socket socket = null;
+        try (ServerSocket serverSocket = new ServerSocket(4444)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        System.out.println("Сервер, напишите свое сообщение");
+                        String msg = sc.nextLine();
+                        System.out.println("Сообщение было отправлено");
+                        out.println(msg);
+                    }
+                }
+            }).start();
+            while (true) {
+                String msg1 = in.nextLine();
+                if (msg1.equals("/end")) {
+                    break;
+                }
+                System.out.println("Клиент: " + msg1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
